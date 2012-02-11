1315e367221ebee78c16e020c0fbb19f8e0e6ace
Wilson Wan <cos.sourcecode@gmail.com>
2012-02-11 11:22:32 +0800

    add erlang design patterns

---
+++ erlang/patterns.erl
@@ -0,0 +1,31 @@
+-module(patterns).
+-export([average/1, double/1, member/2, average2/1]).
+
+%%%%%%%%%%%%%%%%%%%%%%%
+average(X) -> sum(X) / len(X).
+
+sum([H|T]) -> H + sum(T);
+sum([]) -> 0.
+
+len([_|T]) -> 1 + len(T);
+len([]) -> 0.
+
+%%%%%%%%%%%%%%%%%%%%%%%
+double([H|T]) -> [2*H|double(T)];
+% should not be "double([]) -> 0."
+double([]) -> [].
+
+member(H, [H|_]) -> true;
+member(H, [_|T]) -> member(H, T);
+member(_, []) -> false.
+
+
+%%%%%%%%%%%%%%%%%%%%%%%
+% Only traverses the list ONCE
+% Executes in constant space (tail recursive)
+% The variables Length and Sum play the role of accumulators
+% N.B. average([]) is not defined - (you cannot have the average of zero elements) - evaluating average([]) would cause a run-time error
+average2(X) -> average2(X, 0, 0).
+
+average2([H|T], Length, Sum) -> average2(T, Length+1, Sum+H);
+average2([], Length, Sum) -> Sum / Length.
